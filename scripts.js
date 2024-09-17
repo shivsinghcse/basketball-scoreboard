d5d1ddbcd8b6638f145821c4396ed05fd0f85a2b
Shiv Singh <shivsinghcse@gmail.com>
2024-09-18 01:39:09 +0530

    v-0.0.1 released

---
+++ scripts.js
@@ -0,0 +1,61 @@
+let homeScore = 0;
+let guestScore = 0;
+
+let homeScoreBoard = document.getElementById('home-score');
+let guestScoreBoard = document.getElementById('guest-score');
+
+homeScoreBoard.textContent = homeScore;
+guestScoreBoard.textContent = guestScore;
+
+function addScoreOne() {
+    homeScore += 1;
+    homeScoreBoard.textContent = homeScore;
+    changeColor();
+}
+function addScoreTwo() {
+    homeScore += 2;
+    homeScoreBoard.textContent = homeScore;
+    changeColor();
+}
+function addScoreThree() {
+    homeScore += 3;
+    homeScoreBoard.textContent = homeScore;
+    changeColor();
+}
+
+function addScore1() {
+    guestScore += 1;
+    guestScoreBoard.textContent = guestScore;
+    changeColor();
+}
+function addScore2() {
+    guestScore += 2;
+    guestScoreBoard.textContent = guestScore;
+    changeColor();
+}
+function addScore3() {
+    guestScore += 3;
+    guestScoreBoard.textContent = guestScore;
+    changeColor();
+}
+
+function newGame() {
+    homeScore = 0;
+    guestScore = 0;
+    homeScoreBoard.textContent = 0;
+    guestScoreBoard.textContent = 0;
+    homeScoreBoard.style.color = '#fff';
+    guestScoreBoard.style.color = '#fff';
+}
+function changeColor() {
+    if (homeScore > guestScore) {
+        homeScoreBoard.style.color = 'green';
+        guestScoreBoard.style.color = 'red';
+    } else if (homeScore === guestScore) {
+        homeScoreBoard.style.color = 'yellow';
+        guestScoreBoard.style.color = 'yellow';
+    } else {
+        homeScoreBoard.style.color = 'red';
+        guestScoreBoard.style.color = 'green';
+    }
+}
